8f645f82b60929ea54c53db0b60610a324db4512
Godexd <loba4ew.rom@gmail.com>
2021-02-10 16:50:40 +0300

    Task done!

---
+++ application/db/people.py
@@ -0,0 +1,4 @@
+from datetime import datetime as dt
+
+def get_employees():
+    print('Wait please, getting employees on' + str(dt.now().date()))
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+from datetime import datetime as dt
+
+
+def calculate_salary():
+    print('Wait please, calculating salary on ' + str(dt.now().date()))
